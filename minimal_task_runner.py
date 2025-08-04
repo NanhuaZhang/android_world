dabf3d14e9c711a7fd486578668ed46626bf2df1
juzhao <juzhao@appen.com>
2025-08-04 18:43:18 +0800

    fix: adb path

--- minimal_task_runner.py
+++ minimal_task_runner.py
@@ -62,7 +62,8 @@ RETRY_KEYWORDS = [
 def _find_adb_directory() -> str:
   """Returns the directory where adb is located."""
   potential_paths = [
-      os.path.expanduser('/Users/zhaojunjie/Android/platform-tools/adb')
+      os.path.expanduser('~/Library/Android/sdk/platform-tools/adb'),
+      os.path.expanduser('~/Android/Sdk/platform-tools/adb'),
   ]
   for path in potential_paths:
     if os.path.isfile(path):
